1c56ff682d6bf37195d093e3176e56b5f11c376e
Carles Sala <carles@pythiac.com>
2020-04-22 22:14:03 +0200

    Add python3.7 support

--- setup.py
+++ setup.py
@@ -107,6 +107,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     description='Pipelines and primitives for machine learning and data science.',
     entry_points = {
